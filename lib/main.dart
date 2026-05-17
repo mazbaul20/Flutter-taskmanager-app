3254bfc977d871934cf542772691b302adb1f68a
mazbaul20 <mazbaul20@gmail.com>
2026-05-17 20:01:57 +0600

    Arrange Application Routes Navigations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:taskmanager/screen/onboarding/emailVerificationScreen.dart';
+import 'package:taskmanager/screen/onboarding/loginScreen.dart';
+import 'package:taskmanager/screen/onboarding/pinVerificationScreen.dart';
+import 'package:taskmanager/screen/onboarding/registrationScreen.dart';
+import 'package:taskmanager/screen/onboarding/setPasswordScreen.dart';
+import 'package:taskmanager/screen/onboarding/splashScreen.dart';
 
 void main(){
   runApp(const MyApp());
@@ -11,45 +17,20 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue,useMaterial3: false,),
-      home: HomeScreen(),
+      title: "Task Manager",
+      // name route or navigation
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => loginScreen(),
+        '/registration': (context) => registrationScreen(),
+        '/emailVerification': (context) => emailVerificationScreen(),
+        '/pinVerification': (context) => pinVerificationScreen(),
+        '/setPassword': (context) => setPasswordScreen(),
+      },
     );
 
   }
 
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  var count = 0;
-
-  void increment(){
-    setState(() {
-      count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Task Manager'),
-      ),
-      body: Center(
-        child: Text("$count",style: TextStyle(fontSize: 50),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          increment();
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
---
+++ lib/screen/onboarding/splashScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Task Manager"),
+      ),
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
+  }
+}
