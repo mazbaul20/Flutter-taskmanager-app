88b49d30f0e8fe4f57f4adf9bb1833099040d69c
mazbaul20 <mazbaul20@gmail.com>
2026-05-18 18:55:56 +0600

    Initial API Call Methods

---
+++ lib/screen/task/cancelTaskListScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class cancelTaskListScreen extends StatelessWidget {
+  const cancelTaskListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
