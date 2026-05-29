8325926457f344a220d8532a63c9c6818fa7a6ca
mazbaul20 <mazbaul20@gmail.com>
2026-05-29 16:46:47 +0600

    Remove Debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: false,
         // colorScheme: ColorScheme.fromSeed(
